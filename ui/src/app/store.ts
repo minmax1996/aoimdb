f18c274feccffb44f5556b99e077811a5adc929b
MaximMinaev <minmax1996@mail.ru>
2022-11-27 22:27:00 +0400

    React UI v1
    * reactUi tests
    
    * add component for web console
    
    * add clearkeys and simple keys layout
    
    * add get keys template
    
    * add example
    
    * Revert "add example"
    
    This reverts commit 9d4a987a220f07dac2299a64dcfd77f8bb9033f8.
    
    * add bootstrap navbar
    
    * prettier and rename component
    
    * add simple visualization
    
    * update yarn
    
    * upgrade
    
    * npm audit fix
    
    * ?
    
    * Revert "?"
    
    This reverts commit a63c1e091d600a700fc29dd1dc418a48144d49f2.

--- ui/src/app/store.ts
+++ ui/src/app/store.ts
@@ -1,11 +1,15 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import databaseReducer from "../features/data-visualizator/databaseSlice";
+import webconsoleReduser from "../features/web-console/webconsoleSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    database: databaseReducer,
+    cli: webconsoleReduser,
   },
 });
+
+export const selectIsOpen = (state: RootState) => state.cli.isOpen;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
---
+++ ui/src/features/web-console/webconsoleSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface WebCliState {
+  isOpen: boolean;
+}
+
+const initialState: WebCliState = {
+  isOpen: false,
+};
+
+export const webconsoleSlice = createSlice({
+  name: "webconsole",
+  initialState,
+  reducers: {
+    toggleCli: (state) => {
+      state.isOpen = !state.isOpen;
+    },
+  },
+});
+
+export const { toggleCli } = webconsoleSlice.actions;
+
+export default webconsoleSlice.reducer;
